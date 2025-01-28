67d072ed5d965b28cc56a755fe535214bce2e8c2
ArifulSUST49 <sust.arifulislam@gmail.com>
2025-01-28 15:18:32 +0600

    contact page & contact list & service widget completd

--- single.php
+++ single.php
@@ -29,11 +29,13 @@
             <div class="service-box">
               <h4>Services List</h4>
               <div class="services-list">
+                
                 <!-- <a href="#" class="active"><i class="bi bi-arrow-right-circle"></i><span>Web Design</span></a>
                 <a href="#"><i class="bi bi-arrow-right-circle"></i><span>Web Design</span></a>
                 <a href="#"><i class="bi bi-arrow-right-circle"></i><span>Product Management</span></a>
                 <a href="#"><i class="bi bi-arrow-right-circle"></i><span>Graphic Design</span></a>
                 <a href="#"><i class="bi bi-arrow-right-circle"></i><span>Marketing</span></a> -->
+
               <?php dynamic_sidebar('service') ?> 
               
               </div>
