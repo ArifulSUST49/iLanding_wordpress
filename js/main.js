7007175a0dd74c776a4806d75171cad0ee6239de
ArifulSUST49 <sust.arifulislam@gmail.com>
2025-01-29 12:23:55 +0600

    navbar link adjust

--- js/main.js
+++ js/main.js
@@ -37,3 +37,18 @@ jQuery(document).ready(function ($) {
   });
 
   new PureCounter();
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust offset if needed
+                behavior: 'smooth'
+            });
+        });
+    });
+})
